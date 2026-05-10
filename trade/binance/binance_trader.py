5ef32a26ee8628066c323a745d151ced6f810f7d
youse <youseonho@gmail.com>
2026-05-10 18:36:53 +0900

    바이낸스선물 주문체결시간 수신한 데이터로 수정

--- trade/binance/binance_trader.py
+++ trade/binance/binance_trader.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QApplication
 from trade.base_trader import BaseTrader
 from utility.settings.setting_base import UI_NUM
 from utility.static_method.static_decorator import error_decorator
-from utility.static_method.static_datetime import now, timedelta_sec, get_str_ymdhms
+from utility.static_method.static_datetime import now, timedelta_sec, str_ymdhms_utc
 from utility.static_method.static_numba import get_profit_coin_future_short, get_profit_coin_future_long
 
 
@@ -173,8 +173,8 @@ class BinanceTrader(BaseTrader):
             cp = float(data['L'])
             op = float(data['p'])
             on = int(data['i'])
+            ct = str_ymdhms_utc(data['T'])
             if cc > 0 or 'CANCEL' in p:
-                ct = get_str_ymdhms(self.market_gubun)
                 self._update_chejan_data_coin_future(p, code, oc, cc, mc, cp, op, ct, on)
 
     def _get_modify_price(self, 현재가, 정정호가, 종목코드):
