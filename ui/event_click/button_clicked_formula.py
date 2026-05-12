413ee31b45c3aff84198060d8e83c1b167714fa2
youse <youseonho@gmail.com>
2026-05-12 17:55:39 +0900

    수식관리자 수식명에 분석시스템용 팩터명을 사용하지 못하도록 필터 추가

--- ui/event_click/button_clicked_formula.py
+++ ui/event_click/button_clicked_formula.py
@@ -74,10 +74,15 @@ def formula_button_clicked(ui):
         style  = int(ui.fm_comboBoxxxxx_04.currentText()[:1])
         stg    = ui.fm_textEdittttt_01.toPlainText()
 
+        name_blacklist = [
+            '시그널', '신뢰도', '리스크', '패턴점수', '패턴신뢰도', '리스크점수', '거래량점수', '거래량신뢰도', '가격대점수', '가격대신뢰도',
+            '변동성점수', '변동성신뢰도', '예상수익률', '익절수익률', '손절수익률', '변손익신뢰도'
+        ]
+
         if name == '' or stg == '':
             QMessageBox.critical(ui.dialog_formula, '오류 알림', '수식명 또는 수식코드가 공백상태입니다.\n')
             return
-        elif name in dict_stg_name.values():
+        elif name in dict_stg_name.values() or name in name_blacklist:
             QMessageBox.critical(ui.dialog_formula, '오류 알림', '글로벌 함수명과 같은 이름을 사용할 수 없습니다.\n')
             return
         elif vtype in ('선:일반', '선:조건') and width > 5:
