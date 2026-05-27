e420caf484eac597678243ae2fc295041f966a24
youse <youseonho@gmail.com>
2026-05-28 08:46:40 +0900

    야간선물 일자 변경 확인 시간 증가

--- trade/future/future_receiver.py
+++ trade/future/future_receiver.py
@@ -111,6 +111,6 @@ class FutureReceiver(BaseReceiver):
                     self.soundQ.put(text)
 
     def _update_today(self, inthms):
-        if inthms < 10 and not self.update_today:
+        if inthms < 100 and not self.update_today:
             self.str_today = str_ymd(timedelta_day(1, dt_ymd(self.str_today)))
             self.update_today = True
