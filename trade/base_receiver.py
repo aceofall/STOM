567f60cb2e83a43e4286ce416fc4710c06c379b3
youse <youseonho@gmail.com>
2026-05-29 10:10:43 +0900

    업비트 첫틱에 당일매수금액, 당일매도금액 계산 수정 - 0시 정각에 전일 매수/매도수량 데이터가 수신되어 - 해당 시간에 데이터는 무시하도록 변경

--- trade/base_receiver.py
+++ trade/base_receiver.py
@@ -222,8 +222,8 @@ class BaseReceiver:
         if v is None:
             bids_ = round(tbids - pretbids, 8)
             asks_ = round(tasks - pretasks, 8)
-            if bids_ == tbids: bids_ = 0.0
-            if asks_ == tasks: asks_ = 0.0
+            if bids_ < 0 or asks_ < 0:
+                return
         else:
             bids_ = v if cg == '+' else 0
             asks_ = v if cg == '-' else 0
