07ff7116310f3201a99821f342cd21eb515e8b59
youse <youseonho@gmail.com>
2026-07-28 19:08:53 +0900

    수익률 계산 시 위탁증거금율 추출 오류 수정

--- trade/future/future_trader.py
+++ trade/future/future_trader.py
@@ -146,12 +146,12 @@ class FutureTrader(BaseTrader):
 
     def _get_profit_long(self, 매입금액, 보유금액, 보유수량, 종목코드):
         """롱 수익을 계산합니다."""
-        위탁증거금 = int(매입금액 * self.dict_info['위탁증거금율'])
+        위탁증거금 = int(매입금액 * self.dict_info[종목코드]['위탁증거금율'])
         return get_profit_future_long(매입금액, 보유금액, 위탁증거금)
 
     def _get_profit_short(self, 매입금액, 보유금액, 보유수량, 종목코드):
         """숏 수익을 계산합니다."""
-        위탁증거금 = int(매입금액 * self.dict_info['위탁증거금율'])
+        위탁증거금 = int(매입금액 * self.dict_info[종목코드]['위탁증거금율'])
         return get_profit_future_short(매입금액, 보유금액, 위탁증거금)
 
     def _get_hogaunit(self, 주문가격또는종목코드):
