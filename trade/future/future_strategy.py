d2ecc4c2c4744bd6c08e29856c31f5e1127cb01e
youse <youseonho@gmail.com>
2026-07-29 10:01:21 +0900

    전략연산 위탁증거금율 추출 오류 수정

--- trade/future/future_strategy.py
+++ trade/future/future_strategy.py
@@ -16,12 +16,12 @@ class FutureStrategy(BaseStrategy):
 
     def _get_profit_long(self, 매입금액, 보유금액, 보유수량, 종목코드):
         """롱 수익을 계산합니다."""
-        위탁증거금 = int(매입금액 * self.dict_info['위탁증거금율'])
+        위탁증거금 = int(매입금액 * self.dict_info[종목코드]['위탁증거금율'])
         return get_profit_future_long(매입금액, 보유금액, 위탁증거금)
 
     def _get_profit_short(self, 매입금액, 보유금액, 보유수량, 종목코드):
         """숏 수익을 계산합니다."""
-        위탁증거금 = int(매입금액 * self.dict_info['위탁증거금율'])
+        위탁증거금 = int(매입금액 * self.dict_info[종목코드]['위탁증거금율'])
         return get_profit_future_short(매입금액, 보유금액, 위탁증거금)
 
     def _get_hold_time(self, 매수시간):
